044676d478334c68b45d5cdd5fb8968684f46fd7
Cesar <ipog71@gmail.com>
2022-03-09 10:35:56 -0600

    memeadas

--- principal.cpp
+++ principal.cpp
@@ -3,10 +3,25 @@
 #include <time.h>
 #include "pasajero/pasajero.hpp"
 #include "nodo.hpp"
+#include "cola.hpp"
+#include "Pila.hpp"
 
-/*
+using namespace std;
+
+/* 
+*Codigo
+//1.- Pedir nombres
+//2.- Comprar el Ticket almacenando los datos (Encolar)
+//3.- Asignación de Asientos
+//4.- Asignar numero de maletas (random)
+5.- Cola de revisión de maletas(Encolar) (Tardan segun cuanto equipaje tengan)
+6.- Abordar Avión (Apilar)
+7.- Bajar de Avión (Desapilar)
+8.- Abordar taxi (Encolar)
+
+*Grafico
 ////1.- Pedir nombres
-2.- Comprar el Tiket almacenando los datos (Encolar)
+2.- Comprar el Ticket almacenando los datos (Encolar)
 3.- Asignación de Asientos
 4.- Asignar numero de maletas (random)
 5.- Cola de revisión de maletas(Encolar) (Tardan segun cuanto equipaje tengan)
@@ -15,20 +30,33 @@
 8.- Abordar taxi (Encolar)
 */
 using namespace std;
-void asignarMaletas();
-void asignarAsiento();
+void pedirNombres();
+void comprarTicket();
+cola *cola1 = new cola();
 int main()
 {
+    pedirNombres();
+    cola1->comprarTicket();
+    system("PAUSE");
+    return 0;
+}
+void pedirNombres()
+{
     int opc = 1;
+    pasajero pasajerAux;
     do
     {
         cout << "Ingresa el nombre del cliente: \n";
         nodo *nuevo_nodo = new nodo();
-        cin >> nuevo_nodo->dato.nombre;
-        cout << "Quiere agregar un nuevo cliente?\n 1) Si\n Cualquier otra opcion) No\n";
-    } while (opc == 1);
+        cin >> pasajerAux.nombre;
+        cola1->queue(pasajerAux);
+        cout << "Quiere agregar un nuevo cliente?\n 1) Si\n Otro) No\n";
+        cin >>opc;
+    } while (opc != 2);
+}
+void comprarTicket()
+{
 
-    return 0;
 }
 void asignarAsiento()
 {
