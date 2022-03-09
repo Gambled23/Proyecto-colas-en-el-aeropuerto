12a640f1784c3cb76d86bc156813430d730c7dfb
Cesar <ipog71@gmail.com>
2022-03-09 11:02:02 -0600

    Comentarios

--- prueba monito.cpp
+++ prueba monito.cpp
@@ -9,14 +9,16 @@ void gotoxy(int x, int y)
     COORD dwPos;
     dwPos.X = x;
     dwPos.Y = y;
-        SetConsoleCursorPosition(hcon, dwPos);
+    SetConsoleCursorPosition(hcon, dwPos);
 }
 int main(int argc, char **argv)
 {
     system("cls");
     int ban = 0;
-    for (int i = 0; i < 50; i++)
+    for (int i = 0; i < 50; i++) //El 50 es que tanto se mueve a la derecha
     {
+        gotoxy(i, 9); //i es el X, y se suma 1 cada que se mueve, para que se mueva a la derecha
+        cout << "  3"; //Lo que se imprime en esa posicion de x/y
         gotoxy(i, 10);
         cout << "  o";
         gotoxy(i, 11);
@@ -33,9 +35,8 @@ int main(int argc, char **argv)
             cout << "   \\";
             ban = 0;
         }
-        Sleep(100);
+        Sleep(100); //Velocidad con la que se desplaza
     }
-
     system("pause>>cls");
     return 0;
 }
