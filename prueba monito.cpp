22ac635e3823c9a8784b34f05213c0722b3419f9
Cesar <ipog71@gmail.com>
2022-03-09 11:34:04 -0600

    pruebas

--- prueba monito.cpp
+++ prueba monito.cpp
@@ -15,28 +15,55 @@ int main(int argc, char **argv)
 {
     system("cls");
     int ban = 0;
-    for (int i = 0; i < 50; i++) //El 50 es que tanto se mueve a la derecha
+    int j = 0;
+    int lim = 3;
+    int k = 90;
+    int id = 1;
+    do
     {
-        gotoxy(i, 9); //i es el X, y se suma 1 cada que se mueve, para que se mueva a la derecha
-        cout << "  3"; //Lo que se imprime en esa posicion de x/y
-        gotoxy(i, 10);
-        cout << "  o";
-        gotoxy(i, 11);
-        cout << " /| \\";
-        if (ban == 0)
+        for (int i = 0; i < k; i++) // El 50 es que tanto se mueve a la derecha
         {
-            gotoxy(i, 12);
-            cout << " / ";
-            ban = 1;
+            gotoxy(95, 8);
+            cout << "------------------";
+            gotoxy(95, 9);
+            cout << "|";
+            gotoxy(95, 11);
+            cout << "|";
+            gotoxy(95, 12);
+            cout << "|";
+            gotoxy(100, 9);
+            cout << " Tickets";
+            gotoxy(100, 10);
+            cout << "  o";
+            gotoxy(100, 11);
+            cout << " /|\\";
+            gotoxy(100, 12);
+            cout << " / \\";
+            gotoxy(i, 9);  // i es el X, y se suma 1 cada que se mueve, para que se mueva a la derecha
+            cout << "  "<<id; // Lo que se imprime en esa posicion de x/y
+            gotoxy(i, 10);
+            cout << "  o";
+            gotoxy(i, 11);
+            cout << " /| \\";
+            if (ban == 0)
+            {
+                gotoxy(i, 12);
+                cout << " / ";
+                ban = 1;
+            }
+            else
+            {
+                gotoxy(i, 12);
+                cout << "   \\";
+                ban = 0;
+            }
+            Sleep(10); // Velocidad con la que se desplaza
         }
-        else
-        {
-            gotoxy(i, 12);
-            cout << "   \\";
-            ban = 0;
-        }
-        Sleep(100); //Velocidad con la que se desplaza
-    }
+        j++;
+        k -= 7;
+        id++;
+    } while (j != lim);
+
     system("pause>>cls");
     return 0;
 }
