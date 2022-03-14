4f39017fea0970a64f60393ceab2b318a1109b34
Cesar <ipog71@gmail.com>
2022-03-14 10:01:55 -0600

    Prueba para imprimir un taxi

---
+++ carrito.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+int main()
+{
+    cout << "     TAXI" << endl
+         << "    _______" << endl
+         << "   /   ||  \\" << endl
+         << " _/    ||___\\__" << endl
+         << "|  _       _   )" << endl
+         << "|_/ \\_____/ \\__|" << endl
+         << "  \\_/     \\_/" << endl;
+         system("PAUSE");
+    return 0;
+}
